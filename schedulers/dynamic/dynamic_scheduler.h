7c8ddafdf7d7d82cb298774b3f6eefa9f2dc9406
astrlux <37908451+astrlux@users.noreply.github.com>
2023-11-23 18:26:04 -0800

    init header for dyn scheduler

---
+++ schedulers/dynamic/dynamic_scheduler.h
@@ -0,0 +1,14 @@
+#ifndef GHOST_SCHEDULERS_DYNAMIC_DYNAMIC_SCHEDULER_H
+#define GHOST_SCHEDULERS_DYNAMIC_DYNAMIC_SCHEDULER_H
+
+#include <deque>
+#include <memory>
+
+#include "lib/agent.h"
+#include "lib/scheduler.h"
+
+namespace ghost {
+
+}  // namespace ghost
+
+#endif  // GHOST_SCHEDULERS_DYNAMIC_DYNAMIC_SCHEDULER_H
